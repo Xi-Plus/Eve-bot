1926dd740b6112c349ffb5b7a20e87ca3f92dc3c
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-01-24 20:36:24 +0800

    clone all function to func folder

--- config/config_example.php
+++ config/config_example.php
@@ -3,9 +3,9 @@ $config['page_id']='page_id';
 $config['verify_token']='verify_token';
 $config['database_name']='database_name';
 
-$config['sql_path']=__DIR__.'/sql.php';
-$config['curl_path']=__DIR__.'/curl.php';
-$config['facebook_sdk_path']=__DIR__.'/facebook-php-sdk-v4-5.0-dev/src/Facebook/autoload.php';
+$config['sql_path']=__DIR__.'/../function/SQL-function/sql.php';
+$config['curl_path']=__DIR__.'/../function/cURL-HTTP-function/curl.php';
+$config['facebook_sdk_path']=__DIR__.'/../function/facebook-php-sdk-v4/src/Facebook/autoload.php';
 
 $config['app_id']='app_id';
 $config['app_secret']='app_secret';
